009aec2b2bbd0d28b3027335113dc9431477e3aa
lovert-amin <lovmomo700@gmail.com>
2020-01-27 08:13:16 -0800

    starting a new project with just somelines to be updated later

---
+++ lovert/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Body from "./Component/body/Body";
+
+
+
+function App() {
+  return (
+    <div>
+     <Body/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ lovert/src/Component/body/Body.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import "./body.css";
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="image">
+                    <img src="https://robohash.org/test" alt="" />
+                    <div>
+                        <h2>Jane Doe</h2>
+                        <p>jane.doe@gmail.com</p>
+                    </div>
+                </div>
+                <div className="image">
+                    <img src="https://robohash.org/test" alt="" />
+                    <div>
+                        <h2>Jane Doe</h2>
+                        <p>jane.doe@gmail.com</p>
+                    </div>
+                </div>
+                <div className="image">
+                    <img src="https://robohash.org/test" alt="" />
+                    <div>
+                        <h2>Jane Doe</h2>
+                        <p>jane.doe@gmail.com</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        )
+    }
+}
